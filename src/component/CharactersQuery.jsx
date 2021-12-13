b968b4605dbbb332c2cac2a491efefc8b0e8f6fb
wveimar <wveimar.mamian@gmail.com>
2021-12-13 18:56:35 -0500

    listo para Pag

--- src/component/CharactersQuery.jsx
+++ src/component/CharactersQuery.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
-import { Button, Grid} from "@mui/material";
-
-
+import { Button, Grid, Stack } from "@mui/material";
+import { makeStyles } from '@mui/styles';
 import { CardCustom } from "../ui/CardCustom";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        
+        
+        
+    },
+
+    targeta:{
+        margin: 8,
+
+    }
+}));
+
 const CharactersQuery = () => {
-  const [pag,setPag] = useState(3)
-  const { loading, error, data } = useQuery(gql`
+    const classes = useStyles();
+    const [pag, setPag] = useState(3)
+    const { loading, error, data } = useQuery(gql`
     {
       characters (page:${pag}){
         results {
@@ -19,24 +32,26 @@ const CharactersQuery = () => {
       }
     }
   `);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return (
-    <div>
-    
-        {data.characters.results.map((character) => {
-          return (
-            <Grid  container  item xs={6} sm={5} md={4} lg={4} xl={6}>
-             <CardCustom imageJPG={character.image} text="agregar" name={character.name} especies={character.species} key={character.id}/>
-             </Grid>
-             
-          );
-        })}
-      <Button onClick={() =>setPag(pag + 1)}>Next</Button>
-    
-      <Button onClick={() =>setPag(pag - 1)}>Previous</Button>
-    </div>
-  );
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    return (
+        <div className={classes.root} >
+            <Grid container   >
+                {data.characters.results.map((character) => {
+                    return (
+                        <div key={character.id} className={classes.targeta}>
+                            <CardCustom imageJPG={character.image} text="agregar" name={character.name} especies={character.species} />
+                        </div>
+                    );
+                })}
+
+
+            </Grid>
+            <Button onClick={() => setPag(pag + 1)}>Next</Button>
+
+            <Button onClick={() => setPag(pag - 1)}>Previous</Button>
+        </div>
+    );
 };
 
 export default CharactersQuery;
--- src/ui/CardCustom.jsx
+++ src/ui/CardCustom.jsx
@@ -1,46 +1,30 @@
-
-
-
 import { makeStyles } from '@mui/styles';
 import ButtonCustom from './ButtonCustom';
-import { CardMedia, Grid, CardActions, CardContent, Typography } from "@mui/material";
+import { CardMedia, Card, CardActions, CardContent, Typography } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    targeta: {
-        display: 'flex',
-        justifyContent: "center",
-        border: "none",
-        // margin: theme.spacing(1),
-        // boxShadow: theme.shadows[0],
-    },
     boxButton: {
         justifyContent: 'space-evenly',
     },
 }));
 
-export const CardCustom = ({ imageJPG, text,name,especies,key }) => {
+export const CardCustom = ({ imageJPG, text,name,especies }) => {
     const classes = useStyles();
     return (
-
-        <Grid  container className={classes.root} item xs={6} sm={5} md={4} lg={4} xl={4}>
-            <Grid key ={key}>
-                <CardMedia className={classes.targeta} component="img" image={imageJPG} height="300" />
+            <Card sx={{ maxWidth: 345 }} >
+                <CardMedia  component="img" image={imageJPG} height="300" />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         name:{name}
                     </Typography>
-                    <Typography variant="h5" color="text.secondary">
+                    <Typography variant="h6" color="text.secondary">
                         especies:{especies}
                     </Typography>
                 </CardContent>
                 <CardActions className={classes.boxButton}>
                     <ButtonCustom text={text} />
                 </CardActions>
-            </Grid>
-        </Grid>
+            </Card>
+        
     )
 }
